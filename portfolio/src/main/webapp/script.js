63b7603809c708a7f84e1c5edb5769ef43ffa293
Vrusha <vrushashah17@gmail.com>
2022-06-17 17:16:52 +0000

    Adds a translation library to the page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -36,3 +36,23 @@ async function showServerResponse() {
     responseContainer.innerText = textFromResponse;
 }
 
+function requestTranslation() {
+    const text = document.getElementById('text').value;
+    const languageCode = document.getElementById('language').value;
+
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerText = 'Loading...';
+
+    const params = new URLSearchParams();
+    params.append('text', text);
+    params.append('languageCode', languageCode);
+
+    fetch('/translate', {
+      method: 'POST',
+      body: params
+    }).then(response => response.text())
+    .then((translatedMessage) => {
+      resultContainer.innerText = translatedMessage;
+    });
+}
+
